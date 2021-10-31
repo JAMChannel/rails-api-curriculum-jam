c0cecb6d986ded55b004852eb9904580f99ac2c8
JAMChannel <pon244tt@gmail.com>
2021-10-31 23:52:21 +0900

    router導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,14 @@ require("channels")
 
 import Vue from 'vue'
 import App from '../app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify();
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    vuetify,
     render: h => h(App)
   }).$mount()
   // debugger
