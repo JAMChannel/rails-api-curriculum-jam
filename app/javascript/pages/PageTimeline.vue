833042c3e864dc566a94e75e5389c5e9da912dca
JAMChannel <pon244tt@gmail.com>
2021-11-20 18:24:14 +0900

    ページネーション（フロント）

--- app/javascript/pages/PageTimeline.vue
+++ app/javascript/pages/PageTimeline.vue
@@ -2,14 +2,27 @@
     <v-container>
         <v-row>
             <v-col>
-                <div>
+                <div class="pb-10">
                     <div class="mb-5" v-if="$store.getters['auth/currentUser']">
                         <micropost-form @created="createMicropost"></micropost-form>
                     </div>
-                    <timeline-list v-if="isExistMicroposts" :microposts="microposts"></timeline-list>
+                    <timeline-list v-if="isExistMicroposts" :microposts="microposts" class="mb-5"></timeline-list>
                     <div class="text-center" v-else>
                         一件もありません
                     </div>
+
+
+                    <template v-if="pagingMeta">
+                        <div class="text-center">
+                            <v-pagination
+                                    color="indigo"
+                                    v-model="pagingMeta.current_page"
+                                    :length="pagingMeta.total_pages"
+                                    @input="paging"
+                            ></v-pagination>
+                        </div>
+                    </template>
+
                 </div>
             </v-col>
         </v-row>
@@ -24,6 +37,8 @@
         data() {
             return {
                 microposts: [],
+                pagingMeta: null,
+                currentPage: 1
             }
         },
         components: {
@@ -41,8 +56,11 @@
         },
         methods: {
             async fetchMicroposts() {
-                const res = await axios.get(`/api/microposts`)
+                // const res = await axios.get(`/api/microposts`)
+                // debugger
+                const res = await axios.get(`/api/microposts`, { params: { page: this.currentPage } })
                 this.microposts = res.data.microposts
+                this.pagingMeta = res.data.meta
             },
             async createMicropost(micropostContent) {
                 const micropostParams = {
@@ -54,6 +72,12 @@
                 this.microposts =[...[res.data.micropost], ...this.microposts]
                 // debugger
                 
+            },
+            paging(page) {
+            this.currentPage = page
+            this.fetchMicroposts()
+            this.$vuetify.goTo(0)
+            // debugger
             }
         }
     }
